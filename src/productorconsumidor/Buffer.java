d64a282c6cb85251ab88e29e7b7a58b03241fae7
Diego Alonso Martínez de Dios <A01228042@itesm.mx>
2019-04-27 18:13:48 -0500

    Creado el buffer

--- src/productorconsumidor/Buffer.java
+++ src/productorconsumidor/Buffer.java
@@ -5,10 +5,50 @@
  */
 package productorconsumidor;
 
-/**
- *
- * @author DiegoAlonso
- */
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.ArrayList;
+import java.util.LinkedList; 
+import java.util.Queue;
 public class Buffer {
+    
+    Queue<ArrayList> buffer;
+    
+    public Buffer(int bufferSize){
+        this.buffer = new LinkedList<>();
+    }
+    
+    synchronized void produce(ArrayList product){
+         if(this.buffer.peek()!= null) {
+            try {
+                wait(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Buffer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        this.buffer.add(product);
+        
+        notify();
+    }
+    
+    synchronized ArrayList consume(){
+        ArrayList product = new ArrayList();
+        
+        if(this.buffer.peek() == null){
+            try {
+                wait(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Buffer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        product = this.buffer.peek();
+        this.buffer.remove();
+        notify();
+        
+        return product;
+    }
+    
+    
+    
     
 }
