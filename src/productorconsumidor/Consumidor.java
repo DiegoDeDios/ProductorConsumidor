9dd4bd723eeb1a00fe99861ec5b1de0ebfc04c6a
jorgeps14 <jorgeps14@192.168.1.7>
2019-04-27 18:46:15 -0500

    Removed tests - Added operation solving logic

--- src/productorconsumidor/Consumidor.java
+++ src/productorconsumidor/Consumidor.java
@@ -25,6 +25,23 @@ public class Consumidor extends Thread {
             
             System.out.println("Consumer consumed " + product.toString());
             
+            switch((char)product.get(0)) {
+                case '+':
+                    //TODO Access GUI to display result
+                    break;
+                case '-':
+                    //TODO Access GUI to display result
+                    break;
+                case '*':
+                    //TODO Access GUI to display result
+                    break;
+                case '/':
+                    //TODO Access GUI to display result
+                    break;
+                default:
+                    break;
+            }
+            
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException ex) {
--- src/productorconsumidor/ProductorConsumidor.java
+++ src/productorconsumidor/ProductorConsumidor.java
@@ -6,17 +6,9 @@ public class ProductorConsumidor {
 
     public static void main(String[] args) {
         
-        //java.awt.EventQueue.invokeLater(() -> {
-          //  new ProductorConsumidorGUI().setVisible(true);
-        //});
-        
-        Buffer buffer = new Buffer(100);
-        
-        Productor producer = new Productor(1000, buffer);
-        producer.start();
-        
-        Consumidor consumer = new Consumidor(1000, buffer);
-        consumer.start();
+        java.awt.EventQueue.invokeLater(() -> {
+            new ProductorConsumidorGUI().setVisible(true);
+        });
         
     }
     
