1cad468f3e26861538ae8a1a37aeb4a3a13e92b0
jorgeps14 <jorgeps14@192.168.1.7>
2019-04-27 17:40:52 -0500

    Created Productor Class

--- src/productorconsumidor/Productor.java
+++ src/productorconsumidor/Productor.java
@@ -1,14 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package productorconsumidor;
 
-/**
- *
- * @author DiegoAlonso
- */
-public class Productor {
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Productor extends Thread{
+    
+    private long espera = 500;
+    private Buffer buffer;
+    
+    public Productor(long espera, Buffer buffer) {
+        this.espera = espera;
+        this.buffer = buffer;
+    }
+    
+    @Override
+    public void run() {
+        System.out.println("Running Producer...");
+        
+        String operands = "+-*/";
+        
+        Random r = new Random(System.currentTimeMillis());
+        
+        ArrayList product = new ArrayList<>();
+        
+        for(int i=0 ; i<15 ; i++) {
+            
+            product.add(operands.charAt(r.nextInt(4)));
+            product.add(r.nextInt(10));
+            product.add(r.nextInt(10));
+            
+            this.buffer.produce(product);
+            
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Productor.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
     
 }
--- src/productorconsumidor/ProductorConsumidor.java
+++ src/productorconsumidor/ProductorConsumidor.java
@@ -5,9 +5,11 @@ import productorconsumidor.GUI.ProductorConsumidorGUI;
 public class ProductorConsumidor {
 
     public static void main(String[] args) {
+        
         java.awt.EventQueue.invokeLater(() -> {
             new ProductorConsumidorGUI().setVisible(true);
         });
+        
     }
     
 }
